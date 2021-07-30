bc7b3e75b016d1f0a848cccbea3a781b67cc65e0
Steven Grady <gradystevena@gmail.com>
2021-07-30 00:59:50 +0000

    view and edit notes

--- starter/firebaseNotes/public/js/viewNotes.js
+++ starter/firebaseNotes/public/js/viewNotes.js
@@ -1,9 +1,12 @@
+let googleUser;
+let googleUserId;
 window.onload = (event) => {
   // Use this to retain user state between html pages.
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
-      const googleUserId = user.uid;
+      googleUser = user;
+      googleUserId = user.uid;
       getNotes(googleUserId);
     } else {
       // If not logged in, navigate back to login page.
@@ -25,11 +28,11 @@ const renderData = (data) => {
     destination.innerHTML = "";
     for (let key in data) {
         const note = data[key];
-        destination.innerHTML += createCard(note);
+        destination.innerHTML += createCard(note, key);
     }
 };
 
-const createCard = (note) => {
+const createCard = (note, noteId) => {
     return `<div class="column is-one-quarter">
                 <div class="card"> 
                     <header class="card-header"> 
@@ -42,6 +45,60 @@ const createCard = (note) => {
                             ${note.text} 
                         </div>
                     </div> 
+                    <footer class="card-footer">
+                    <a 
+                    href="#" 
+                    class="card-footer-item" 
+                    onclick="editNote('${noteId}')"> 
+                    Edit </a>
+                    <a 
+                    href="#" 
+                    class="card-footer-item" 
+                    onclick="deleteNote('${noteId}')"> 
+                    Delete </a>
+                    </footer>
                 </div>
             </div>`;
+};
+
+const deleteNote = (noteId) =>{
+    console.log("Delete Note")
+    const noteToDeleteRef = firebase.database().ref(`users/${googleUser.uid}/${noteId}`);
+    noteToDeleteRef.remove()
+}
+
+const editNote = (noteId) =>{
+    console.log(`Note (${noteId}) edited`);
+    const noteToEditRef = firebase.database().ref(`users/${googleUser.uid}/${noteId}`);
+    noteToEditRef.once('value', (snapshot) => {
+    const note = snapshot.val();
+    const editNoteModal = document.querySelector("#editNoteModal");
+    const editNoteTitleInput = document.querySelector('#editTitleInput');
+    editNoteTitleInput.value = note.title;
+
+    const editNoteTextInput = document.querySelector('#editTextInput');
+    editNoteTextInput.value = note.text;
+
+    editNoteModal.classList.add('is-active');
+    });
+    
+};
+
+const closeModal = () =>{
+     const editNoteModal = document.querySelector("#editNoteModal");
+     editNoteModal.classList.remove('is-active');
+};
+
+const saveChanges = () =>{
+   console.log("Save changes");
+    const noteTitle = document.querySelector("#editTitleInput").value;
+    const noteText = document.querySelector("#editTextInput").value;
+    const noteId = document.querySelector("#editNoteId").value;
+    
+    firebase.database().ref(`users/${googleUserId}/${noteId}`).update({
+        title: notetitle,
+        text: notetext,        
+    });
+
+    closeModal();
 };
